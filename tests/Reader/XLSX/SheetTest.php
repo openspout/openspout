f88c19f2c580e4c3c8bdd979d18b368fc2485d66
Andrey Shertsinger <andrey@shertsinger.ru>
2024-06-06 03:34:55 +0700

    [XLSX] Add mergeCells reader tests

--- tests/Reader/XLSX/SheetTest.php
+++ tests/Reader/XLSX/SheetTest.php
@@ -36,15 +36,48 @@ final class SheetTest extends TestCase
         static::assertTrue($sheets[1]->isVisible());
     }
 
+    public function testReaderShouldReturnEmptySheetMergedCellsByDefault()
+    {
+        /** @var Sheet[] $sheets */
+        $sheets = $this->openFileAndReturnSheets('two_sheets_with_merged_cells.xlsx');
+
+        static::assertEmpty($sheets[0]->getMergeCells());
+        static::assertEmpty($sheets[1]->getMergeCells());
+    }
+
+    public function testReaderShouldReturnCorrectSheetMergedCells()
+    {
+        /** @var Sheet[] $sheets */
+        $sheets = $this->openFileAndReturnSheets('two_sheets_with_merged_cells.xlsx', true);
+        $mergedCellsExpected = [
+            ['A1:B1', 'A2:A3', 'C3:E5', 'B2:E2'],
+            ['A1:A4', 'A5:D5', 'E2:E5', 'C1:E1', 'B1:B3', 'B4:C4', 'D3:D4', 'C2:D2'],
+        ];
+        $mergedCellsActual = [
+            $sheets[0]->getMergeCells(),
+            $sheets[1]->getMergeCells(),
+        ];
+
+        static::assertSameSize($mergedCellsExpected[0], $mergedCellsActual[0]);
+        static::assertEmpty(array_diff($mergedCellsActual[0], $mergedCellsExpected[0]), 'There should be no difference between merged cells on first sheet');
+        static::assertEmpty(array_diff($mergedCellsExpected[0], $mergedCellsActual[0]), 'There should be no difference between merged cells on first sheet');
+        static::assertSameSize($mergedCellsExpected[1], $mergedCellsActual[1]);
+        static::assertEmpty(array_diff($mergedCellsActual[1], $mergedCellsExpected[1]), 'There should be no difference between merged cells on second sheet');
+        static::assertEmpty(array_diff($mergedCellsExpected[1], $mergedCellsActual[1]), 'There should be no difference between merged cells on second sheet');
+    }
+
     /**
      * @param string $fileName
      *
      * @return SheetInterface[]
      */
-    private function openFileAndReturnSheets($fileName)
+    private function openFileAndReturnSheets($fileName, $withMergedCells = false)
     {
         $resourcePath = $this->getResourcePath($fileName);
         $reader = ReaderEntityFactory::createXLSXReader();
+        if ($withMergedCells) {
+            $reader->setShouldLoadMergeCells(true);
+        }
         $reader->open($resourcePath);
 
         $sheets = [];
