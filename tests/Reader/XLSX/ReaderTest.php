d60ffe9ba033050b32b5f50110ca602a35af41d7
Albert Prat <albert.prat@interactiu.cat>
2026-03-09 11:50:06 +0100

    fix: ignore `spans` attribute when initializing row cells if `numColumns` contains value
    If there's already a number of columns computed, either by
    `<dimensions>` tag or any other method, ignore `spans` attribute.
    
    The `spans` attribute on a `<row>` tag is only an optimization to know
    where the data is, in the form of `spans="<minColumn>:<maxColumn>"`,
    but it doesn't delimit the row to these columns.

--- tests/Reader/XLSX/ReaderTest.php
+++ tests/Reader/XLSX/ReaderTest.php
@@ -421,6 +421,22 @@ final class ReaderTest extends TestCase
         self::assertSame($expectedRows, $allRows);
     }
 
+    public function testReadShouldKeepEmptyCellsAtTheEndIfDimensionsAndNarrowerSpansSpecified(): void
+    {
+        $allRows = $this->getAllRowsForFile('sheet_with_dimensions_and_narrower_spans_and_empty_cells.xlsx');
+
+        self::assertCount(2, $allRows, 'There should be 2 rows');
+        foreach ($allRows as $row) {
+            self::assertCount(5, $row, 'There should be 5 cells for every row, because empty cells should be preserved');
+        }
+
+        $expectedRows = [
+            ['s1--A1', 's1--B1', 's1--C1', 's1--D1', 's1--E1'],
+            ['s1--A2', 's1--B2', 's1--C2', '', ''],
+        ];
+        self::assertSame($expectedRows, $allRows);
+    }
+
     public function testReadShouldKeepEmptyCellsAtTheEndIfNoDimensionsButSpansSpecified(): void
     {
         $allRows = $this->getAllRowsForFile('sheet_without_dimensions_and_empty_cells.xlsx');
