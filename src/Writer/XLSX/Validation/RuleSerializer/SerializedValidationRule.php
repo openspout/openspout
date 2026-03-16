f319d7c7d9b8243c9ae3091643e2ee8eaed3d676
Michiel Maas <michiel.maas@moxio.com>
2026-03-16 13:21:27 +0100

    Created ValidationRuleSerializer.php to serialize the ValidationRules to that they can we written to the XML

---
+++ src/Writer/XLSX/Validation/RuleSerializer/SerializedValidationRule.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenSpout\Writer\XLSX\Validation\RuleSerializer;
+
+final readonly class SerializedValidationRule
+{
+    public function __construct(
+        public string $type,
+        public ?string $operator,
+        public string $formula1,
+        public ?string $formula2 = null,
+    ) {}
+}
