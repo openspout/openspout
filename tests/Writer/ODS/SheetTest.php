747e807b32df6b3b17d9c5c70cdb1afb630182c1
Wayne Yang <zxc072211@gmail.com>
2026-05-25 23:47:03 +0800

    fix: ODS column width range handling

--- tests/Writer/ODS/SheetTest.php
+++ tests/Writer/ODS/SheetTest.php
@@ -5,9 +5,14 @@ declare(strict_types=1);
 namespace OpenSpout\Writer\ODS;
 
 use OpenSpout\Common\Entity\Row;
+use OpenSpout\Common\Exception\IOException;
+use OpenSpout\Reader\Exception\XMLProcessingException;
+use OpenSpout\Reader\Wrapper\XMLReader;
 use OpenSpout\TestUsingResource;
 use OpenSpout\Writer\Common\Entity\Sheet;
 use OpenSpout\Writer\Exception\InvalidSheetNameException;
+use OpenSpout\Writer\Exception\WriterNotOpenedException;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -117,6 +122,163 @@ final class SheetTest extends TestCase
         self::assertStringContainsString('style:column-width="100pt"', $xmlContents, 'No cols tag found in sheet');
         self::assertStringContainsString('table:number-columns-repeated="1"', $xmlContents, 'No expected column width definition found in sheet');
         self::assertStringContainsString('table:number-columns-repeated="2"', $xmlContents, 'No expected column width definition found in sheet');
+    }
+
+    /**
+     * @param array<array-key, array{start: positive-int, end: positive-int, width: float}> $rules
+     * @param array<array-key, array{repeated: string, style: string, width?: string}>      $expected
+     *
+     * @throws IOException
+     * @throws XMLProcessingException
+     * @throws WriterNotOpenedException
+     */
+    #[DataProvider('provideWritesColumnWidthsInVariousScenariosCases')]
+    public function testWritesColumnWidthsInVariousScenarios(array $rules, array $expected): void
+    {
+        $fileName = 'test_column_widths_in_various_scensarios.ods';
+        $resourcePath = (new TestUsingResource())->getGeneratedResourcePath($fileName);
+
+        $options = new Options(tempFolder: (new TestUsingResource())->getTempFolderPath());
+        $writer = new Writer($options);
+        $writer->openToFile($resourcePath);
+        $writer->addRow(Row::fromValues(['ods--11', 'ods--12', 'ods--13', 'ods--14', 'ods--15', 'ods--16']));
+        foreach ($rules as $rule) {
+            $options->setColumnWidthForRange(...$rule);
+        }
+        $writer->close();
+
+        $reader = new XMLReader();
+        $reader->openFileInZip($resourcePath, 'content.xml');
+
+        $styles = [];
+        $columns = [];
+
+        while ($reader->read()) {
+            if (XMLReader::ELEMENT !== $reader->nodeType) {
+                continue;
+            }
+            if ('style:style' === $reader->name
+                && 'table-column' === $reader->getAttribute('style:family')
+                && str_starts_with($styleName = $reader->getAttribute('style:name'), 'co')
+            ) {
+                $styles[$styleName] = $reader->readInnerXml();
+            } elseif ('table:table-column' === $reader->name) {
+                $columns[] = [
+                    'repeated' => $reader->getAttribute('table:number-columns-repeated'),
+                    'style' => $reader->getAttribute('table:style-name'),
+                ];
+            }
+        }
+
+        self::assertCount(\count($expected), $columns);
+
+        foreach ($expected as $index => $expectedAttributes) {
+            self::assertSame($expectedAttributes['repeated'], $columns[$index]['repeated']);
+            self::assertSame($expectedAttributes['style'], $columns[$index]['style']);
+            if (isset($expectedAttributes['width'])) {
+                self::assertStringContainsString(
+                    \sprintf('style:column-width="%s"', $expectedAttributes['width']),
+                    $styles[$expectedAttributes['style']]
+                );
+            }
+        }
+    }
+
+    public static function provideWritesColumnWidthsInVariousScenariosCases(): iterable
+    {
+        return [
+            'partial overlap' => [
+                [
+                    ['start' => 1, 'end' => 10, 'width' => 50.0],
+                    ['start' => 5, 'end' => 7, 'width' => 100.0],
+                ],
+                [
+                    ['repeated' => '4', 'style' => 'co0', 'width' => '50pt'],
+                    ['repeated' => '3', 'style' => 'co1', 'width' => '100pt'],
+                    ['repeated' => '3', 'style' => 'co2', 'width' => '50pt'],
+                ],
+            ],
+
+            'nested overlap' => [
+                [
+                    ['start' => 1, 'end' => 10, 'width' => 50.0],
+                    ['start' => 2, 'end' => 8, 'width' => 100.0],
+                    ['start' => 4, 'end' => 6, 'width' => 80.0],
+                ],
+                [
+                    ['repeated' => '1', 'style' => 'co0', 'width' => '50pt'],
+                    ['repeated' => '2', 'style' => 'co1', 'width' => '100pt'],
+                    ['repeated' => '3', 'style' => 'co2', 'width' => '80pt'],
+                    ['repeated' => '2', 'style' => 'co3', 'width' => '100pt'],
+                    ['repeated' => '2', 'style' => 'co4', 'width' => '50pt'],
+                ],
+            ],
+
+            'no overlap' => [
+                [
+                    ['start' => 1, 'end' => 3, 'width' => 50.0],
+                    ['start' => 5, 'end' => 7, 'width' => 100.0],
+                ],
+                [
+                    ['repeated' => '3', 'style' => 'co0', 'width' => '50pt'],
+                    ['repeated' => '1', 'style' => 'default-column-style'],
+                    ['repeated' => '3', 'style' => 'co1', 'width' => '100pt'],
+                ],
+            ],
+
+            'identical intervals' => [
+                [
+                    ['start' => 1, 'end' => 10, 'width' => 50.0],
+                    ['start' => 1, 'end' => 10, 'width' => 100.0],
+                ],
+                [
+                    ['repeated' => '10', 'style' => 'co0', 'width' => '100pt'],
+                ],
+            ],
+
+            'laster interval contains previous' => [
+                [
+                    ['start' => 5, 'end' => 7, 'width' => 100.0],
+                    ['start' => 1, 'end' => 10, 'width' => 50.0],
+                ],
+                [
+                    ['repeated' => '10', 'style' => 'co0', 'width' => '50pt'],
+                ],
+            ],
+
+            'adjacent intervals' => [
+                [
+                    ['start' => 1, 'end' => 5, 'width' => 50.0],
+                    ['start' => 6, 'end' => 10, 'width' => 100.0],
+                ],
+                [
+                    ['repeated' => '5', 'style' => 'co0', 'width' => '50pt'],
+                    ['repeated' => '5', 'style' => 'co1', 'width' => '100pt'],
+                ],
+            ],
+
+            'boundary overlap' => [
+                [
+                    ['start' => 1, 'end' => 5, 'width' => 50.0],
+                    ['start' => 5, 'end' => 10, 'width' => 100.0],
+                ],
+                [
+                    ['repeated' => '4', 'style' => 'co0', 'width' => '50pt'],
+                    ['repeated' => '6', 'style' => 'co1', 'width' => '100pt'],
+                ],
+            ],
+
+            'single point interval' => [
+                [
+                    ['start' => 5, 'end' => 5, 'width' => 50.0],
+                ],
+                [
+                    ['repeated' => '4', 'style' => 'default-column-style'],
+                    ['repeated' => '1', 'style' => 'co0', 'width' => '50pt'],
+                    ['repeated' => '1', 'style' => 'default-column-style'],
+                ],
+            ],
+        ];
     }
 
     private function writerForFile(string $fileName): Writer
